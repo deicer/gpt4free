c5ee3a766dbae83efdd4a66ba41c75ee504303ec
Gennady Lebedev <gena-lebedev@yandex.ru>
2023-04-27 13:32:36 +0700

    Update streamlit_app.py

--- gui/streamlit_app.py
+++ gui/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import phind
 
 phind.cf_clearance = 'ovUVV2HIyt2RPwoy122qMcRRZhZOtFFGEZA1pcl13ns-1682575351-0-160'
-phind.user_agent   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.4.508 (beta) Yowser/2.5 Safari/537.36'
+phind.user_agent   = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 
 def phind_get_answer(question:str)->str:
     # set cf_clearance cookie
